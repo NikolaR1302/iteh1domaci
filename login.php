f8721722dcdf3e78e4f62adb7bbbe6adb0fcd83b
NikolaR1302 <radenovicnikola99@gmail.com>
2022-01-17 19:31:26 +0100

    Login css

--- login.php
+++ login.php
@@ -30,22 +30,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
     <title>Movies to watch</title>
 
 </head>
 <body>
-    <div class="login-form">
-        <div class="main-div">
-            <form method="GET" action="#"> 
-                <h1>Movies to watch</h1>
-                <div class="container">
-                    <input type="text" placeholder="username" name="username" class="login-input"  required>
-                    <input type="password" placeholder="password" name="password" class="login-input" required>
-                    <button type="submit" class="login-button" name="submit">Log in</button> 
-                </div>
-
-            </form>
+        <div class="login-box">
+        <form method="GET" action="#"> 
+        <h2>Login</h2>
+        <form>
+            <div class="user-box">
+            <input type="text" name="username" required>
+            <label>Username</label>
+            </div>
+            <div class="user-box">
+            <input type="password" name="password" required>
+            <label>Password</label>
+            </div>
+            <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <button type="submit" class="login-button" name="submit">Log in</button> 
+            </a>
+        </form>
+        </form>
         </div>
-    </div>
+
+    
 </body>
 </html>
